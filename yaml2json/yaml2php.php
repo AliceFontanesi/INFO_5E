78db3c8d1134b1b8ab0ac31b8aef93e37d11b042
AliceFontanesi <94352195+AliceFontanesi@users.noreply.github.com>
2023-09-28 22:35:27 +0200

    es completato

---
+++ yaml2json/yaml2php.php
@@ -0,0 +1,23 @@
+<?php
+require 'vendor/autoload.php'; // Assicurati di avere la libreria Symfony YAML installata
+
+use Symfony\Component\Yaml\Yaml;
+
+//file yaml input
+$inputYamlFile = 'input.yaml';
+
+//carico contenuto file
+$yamlContent = file_get_contents($inputYamlFile);
+
+//conversione in array
+$arrayData = Yaml::parse($yamlContent);
+
+//conversione in json
+$jsonData = json_encode($arrayData, JSON_PRETTY_PRINT);
+
+$outputJsonFile = 'output.json';
+
+//file json
+file_put_contents($outputJsonFile, $jsonData);
+
+echo "Conversione da YAML a JSON completata\n";
